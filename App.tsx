c8343e6df745198e4e602b00ba5e6c0f4b7aa8f0
xiaowei.xue <xiaowei.xue@shopee.com>
2020-03-15 14:22:29 +0800

    chore: react native css module config

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { BlueText } from './src/component/BlueText';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello world.</Text>
+      <BlueText />
     </View>
   );
 }
@@ -12,6 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
---
+++ src/component/BlueText/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Text } from 'react-native';
+import styles from './index.less';
+
+export const BlueText = () => {
+  return <Text className={styles.blue}>Blue Text</Text>
+}
