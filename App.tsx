53676b3755f36793dee079b95d266c43b56e6a6c
xiaowei.xue <xiaowei.xue@shopee.com>
2020-03-15 15:29:49 +0800

    demos:

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { BlueText } from './src/component/BlueText';
 import { GreenText } from './src/component/GreenText';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello world.</Text>
       <BlueText />
       <GreenText />
-    </View>
+    </SafeAreaView>
   );
 }
 
